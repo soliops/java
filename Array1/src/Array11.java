253beac743195e28cfa7b883f618dc1deb8e300f
soliops <106052857+soliops@users.noreply.github.com>
2022-06-02 16:29:18 +0900

    Add files via upload

--- Array1/src/Array11.java
+++ Array1/src/Array11.java
@@ -11,7 +11,22 @@ public class Array11 {
 		 *  결과 배열에 따른 조건은 60점 이상(포함) 중 합격자만 출력합니다.
 		 * 결과 배열 [hong,kim,lee,soun]
 		 */
-
+		String a[]=	{"hong","kim","park","jang","lee","soun"};
+		int aa[]={80,100,46,38,65,88};
+			
+		int b=a.length;
+		int c=aa.length;
+		int w=0;
+		do {
+			int ww=0;
+			do {
+				if(aa[c]>=60) {
+					System.out.println(a[w]);
+				}
+				ww++;
+			}while(ww<c);
+			w++;
+		}while(w<b);
 	}
 
 }
---
+++ Class/src/Class_array2.java
@@ -0,0 +1,18 @@
+
+public class Class_array2 {
+
+	public static void main(String[] args) {
+
+		//memorys m = new memorys(); -> 메모리보단 출력을 우선
+		memorys m =null;
+		m = new memorys();
+		m.ms();
+		
+	}
+
+}
+class memorys{
+	public void ms() {
+		System.out.println("test");
+	}
+}

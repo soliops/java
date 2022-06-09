781fcba8d6a613417ba50a9fdcbdf720122ec0f3
soliops <106052857+soliops@users.noreply.github.com>
2022-06-09 14:28:00 +0900

    Add files via upload

---
+++ java3/Awt/src/awt1.java
@@ -0,0 +1,11 @@
+
+public class awt1 {
+
+	public static void main(String[] args) {
+		//awt 积己规侥
+		//frame -> Panel -> Object (积己)
+		//Object ->Panel -> frame (利侩)
+		//http://docs.oracle.com/javase/7/docs/api
+	}
+
+}

10cbc0e63f3b71499e64b60fc930b1c52de47c80
soliops <106052857+soliops@users.noreply.github.com>
2022-06-14 17:30:26 +0900

    Add files via upload

--- Oop/src/oop1.java
+++ Oop/src/oop1.java
@@ -1,7 +1,34 @@
 import java.util.Scanner;
 
 public class oop1 {
+/*OOP: 객체지향 프로그래밍 
+[캡슐화+은닉화]
+public, private, protected, [getter, setter](간접적으로 접근)
+[추상화] - abstraction
+코드를 정형화
+개별적인 메소드를 막음
+확장성(interface)
+추상 클래스 + interface
 
+[다향성]-overriding, overloading
+overriding - 부모의 메소드, 변수 상속
+overloading - 자식 클래스에서 메소드를 상속은 받지만 커스텀
+
+[상속성,재사용] - extend(부모+자식), implement(부모 다수 + 자식)
+재사용-재귀함수, interface를 다른 class에서도 재사용
+
+[Thread]
+
+[Multi Thread]
+
+Package 관련
+
+Timeloop try, catch
+
+Arrays
+
+동기화, 비동기화
+ */
 	public static void main(String[] args) {
 		// 재귀메소드 사용법
 		op_method om = new op_method();
